a7a6a2809eafbb139ae4a06d45173e6223b5aecb
Aqsa Amjad <aqsa_amjad@outlook.com>
2024-10-16 10:16:37 +0100

    implemented add() function
    this function takes two integers as input and returns the sum

--- Team28/src/week4/Math.java
+++ Team28/src/week4/Math.java
@@ -1,5 +1,12 @@
 package week4;
 
 public class Math {
-
+    
+/* Developer: Aqsa Amjad
+ * University ID: 230066670
+ * Function: This function takes two inputs as integers and returns the sum
+ */
+public int add(int num1, int num2) {
+    return num1 + num2;
+}
 }
