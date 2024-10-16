6d16861f2aac127b6a31f8190b830dfaa4923096
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2024-10-16 10:20:30 +0100

    Formatting for add()
    Being nit picky with indents lmao

--- Team28/src/week4/Math.java
+++ Team28/src/week4/Math.java
@@ -2,11 +2,13 @@ package week4;
 
 public class Math {
     
-/* Developer: Aqsa Amjad
- * University ID: 230066670
- * Function: This function takes two inputs as integers and returns the sum
- */
-public int add(int num1, int num2) {
-    return num1 + num2;
-}
+	/* Developer: Aqsa Amjad
+	 * University ID: 230066670
+	 * Function: This function takes two inputs as integers and returns the sum
+	 */
+	public int add(int num1, int num2) {
+	    return num1 + num2;
+	}
+
+
 }
