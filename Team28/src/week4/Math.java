18af1191f8d7b889100624dd40b90829738364f6
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2024-10-16 15:07:34 +0100

    readding mod
    Essta's commit removed the mod function for some reason?

--- Team28/src/week4/Math.java
+++ Team28/src/week4/Math.java
@@ -31,5 +31,13 @@ public class Math {
 	public int sub(int num1, int num2) {
     return num1 - num2;
 	}
+	
+	/* Developer: Vatsal Mehta
+	 * University ID: 220408633
+	 * Function: This function takes two integer as input, calculate the mod ("%") the first number by second number and returns the result. For example Mod(8,2) should return 0, whereas Mod(7,2) should return 1.
+	 */
+	public int Mod(int a, int b) {
+		return a % b;
+	}
 
 }
